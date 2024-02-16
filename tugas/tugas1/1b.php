dfd53ec0ffdbac7c8fd4c0b43f33449be8f8eb02
Ismail457 <74404097+Ismail4537@users.noreply.github.com>
2024-02-16 13:50:35 +0700

    Tugas1

--- tugas/tugas1/1b.php
+++ tugas/tugas1/1b.php
@@ -1,8 +1,8 @@
 <?php
 $npm = 70;
-echo "Aku adalah $npm <br>";
-echo "Jika aku dikali 5, maka aku sekarang menjadi ". $npm*5 ."<br>";
-echo "Jika aku dibagi 2, maka aku sekarang menjadi ". $npm*5/2 ."<br>";
-echo "Jika aku ditambah 75, maka aku sekarang menjadi ". $npm*5/2+75 ."<br>";
-echo "Jika aku dikurang 20, maka aku sekarang menjadi ". $npm*5/2+75-20 ."<br>";
+echo "Aku adalah<b> $npm</b><br>";
+echo "Jika aku dikali 5, maka aku sekarang menjadi <b>". $npm*5 ."</b><br>";
+echo "Jika aku dibagi 2, maka aku sekarang menjadi <b>". $npm*5/2 ."</b><br>";
+echo "Jika aku ditambah 75, maka aku sekarang menjadi <b>". $npm*5/2+75 ."</b><br>";
+echo "Jika aku dikurang 20, maka aku sekarang menjadi <b>". $npm*5/2+75-20 ."</b><br>";
 ?>
