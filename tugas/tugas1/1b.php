3f8e1c61921bfbbbb163c0c65556610f9d8302d9
Ismail457 <74404097+Ismail4537@users.noreply.github.com>
2024-02-16 13:46:04 +0700

    Tugas1

---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,8 @@
+<?php
+$npm = 70;
+echo "Aku adalah $npm <br>";
+echo "Jika aku dikali 5, maka aku sekarang menjadi ". $npm*5 ."<br>";
+echo "Jika aku dibagi 2, maka aku sekarang menjadi ". $npm*5/2 ."<br>";
+echo "Jika aku ditambah 75, maka aku sekarang menjadi ". $npm*5/2+75 ."<br>";
+echo "Jika aku dikurang 20, maka aku sekarang menjadi ". $npm*5/2+75-20 ."<br>";
+?>
---
+++ tugas/tugas1/1c.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .box{
+            width: 100px;
+            height: 100px;
+            background-color: rgb(255, 116, 116);
+            margin: 10px;
+            text-align: center;
+            line-height: 100px;
+            font-size: 20px;
+            margin: 0;
+            border: 2px solid black;
+        }
+        .line{
+            display: flex;
+        }
+    </style>
+</head>
+<body>
+    <div class="line">
+        <div class="box">1</div>
+    </div>
+    <div class="line">
+        <div class="box">2</div>
+        <div class="box">2</div>
+    </div>
+    <div class="line">
+        <div class="box">3</div>
+        <div class="box">3</div>
+        <div class="box">3</div>
+    </div>
+</body>
+</html>
