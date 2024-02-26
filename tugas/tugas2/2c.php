f48c6c8d1a1f923b43c1d5e9134a9f4cb937fe00
Ismail5478 <74404097+Ismail4537@users.noreply.github.com>
2024-02-26 18:16:11 +0700

    commit

--- tugas/tugas2/2c.php
+++ tugas/tugas2/2c.php
@@ -1,7 +1,15 @@
 <?php
 for ($i = 10; $i >= 1; $i--) {
 	for ($j = 1; $j <= $i; $j++) {
-		echo "<div class='box' style='display: inline-block; width: 50px; height: 50px; border: 1px solid black; text-align: center; background-color: salmon; line-height: 50px;'>$j</div>";
+		echo "<div class='box' 
+		style='
+			display: inline-block; 
+			width: 50px; 
+			height: 50px; 
+			border: 1px solid black; 
+			text-align: center; 
+			background-color: salmon; 
+			line-height: 50px;'>$j</div>";
 	}
 	echo "<br>";
 }
