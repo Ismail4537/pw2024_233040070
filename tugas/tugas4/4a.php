717dae66230a2991103cbf4c360779c8e7085ee8
Ismail5478 <ismail.233040070@mail.unpas.ac.id>
2024-03-05 10:23:35 +0700

    Tugas 4

--- tugas/tugas4/4a.php
+++ tugas/tugas4/4a.php
@@ -1,9 +1,4 @@
 <?php
 $kata = ["epan", "cipta", "akan", "u", "asa", "esok"];
 
-echo "m" . $kata[4] .
-    " d" . $kata[0] .
-    "mu di" . $kata[1] .
-    "kan oleh apa yang kau kerj" . $kata[2] .
-    " hari ini, b" . $kata[3] .
-    "kan b" . $kata[5] . "<br>";
+echo "m$kata[4] d$kata[0]mu di$kata[1]kan oleh apa yang kau kerj$kata[2] hari ini, b$kata[3]kan b$kata[5] <br>";
